987dd41de3abb73c99ac368e0a1d5a412204f08e
changbai-phu <baichang2025@gmail.com>
2024-08-11 16:39:12 +0800

    Update run_script.sh
    update run_script.sh path values

--- run_script.sh
+++ run_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Path to the Python virtual environment
-VENV_PATH="/Users/peggyhu/Downloads/vscode/CurrencyMonitor/venv"
+# Path to the Python virtual environment - input your own path below
+VENV_PATH="/Users/Downloads/vscode/CurrencyMonitor/venv"
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
-# Path to the Python script
-PYTHON_SCRIPT="/Users/peggyhu/Downloads/vscode/CurrencyMonitor/CERM_daily_report.py"
+# Path to the Python script - input your own path below
+PYTHON_SCRIPT="/Users/Downloads/vscode/CurrencyMonitor/CERM_daily_report.py"
 
 # Run the Python script
 python "$PYTHON_SCRIPT"
